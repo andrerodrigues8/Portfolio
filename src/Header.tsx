c9450d3ebabcc5e6e8807b633f550ab9ba7b7aa6
André Rodrigues <uc2021222078@student.uc.pt>
2026-04-24 13:06:55 +0100

    Added header and background gradient

---
+++ src/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div className="pt-4">
+      <h1 className="text-5xl font-bold text-white font-raleway text-center">
+        André Nogueira Rodrigues
+      </h1>
+    </div>
+  );
+}
+export default Header;
